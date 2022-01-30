74e7cc747e37aef5cde5ba45a414c72527d8a912
aadityabhusal <bhusal.001aditya@gmail.com>
2022-01-30 16:50:28 +0545

    feature: creates index file for exporting components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Header } from "./Header";
+export { LinkBar } from "./LinkBar";
+export { MainPanel } from "./MainPanel";
+export { NFTDisplay } from "./NFTDisplay";
+export { Utility } from "./Utility";
