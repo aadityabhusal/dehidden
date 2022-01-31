fa6976479f11910fd4ab2b061f2c87e6239cee54
aadityabhusal <bhusal.001aditya@gmail.com>
2022-01-31 10:38:17 +0545

    refactor: improved utility styling

--- src/components/Utility.js
+++ src/components/Utility.js
@@ -3,10 +3,10 @@ import style from "../modules/mainPanel.module.css";
 export function Utility({ data }) {
   return (
     <a className={style.utility} href={data.url}>
-      <div className={style.icon}>
+      <div className={style.utilityIcon}>
         <img src={`/images/${data.image}`} alt={data.title} />
       </div>
-      <div>
+      <div className={style.utilityText}>
         <div className={style.heading}>{data.title}</div>
         <div className={style.subHeading}>{data.description}</div>
       </div>
