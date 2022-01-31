ed22ce45b328bf4d9407d51cdf81654469a3a6c7
aadityabhusal <bhusal.001aditya@gmail.com>
2022-01-31 12:23:29 +0545

    feature: creates linktext fetching functionality

--- src/components/LinkBar.js
+++ src/components/LinkBar.js
@@ -1,10 +1,27 @@
 import { Button } from "./ui";
 import style from "../modules/header.module.css";
+import { useEffect, useState } from "react";
+const url = "https://run.mocky.io/v3/8321cc85-81e6-45c6-bd23-33e85c504c7d";
 
 export function LinkBar() {
+  const [linkText, setLinkText] = useState("Loading...");
+
+  useEffect(() => {
+    async function fetchText() {
+      try {
+        let response = await (await fetch(url)).text();
+        let result = JSON.parse(response.replace(/“|”/gi, '"'));
+        setLinkText(result.url);
+      } catch (error) {
+        setLinkText("URL not found");
+      }
+    }
+    fetchText();
+  }, []);
+
   return (
     <div className={style.linkbar}>
-      <div className={style.linktext}>https://de.dehidden.com/qwerty</div>
+      <div className={style.linktext}>{linkText}</div>
       <Button icon="copy" text="Copy" isPrimary />
     </div>
   );
